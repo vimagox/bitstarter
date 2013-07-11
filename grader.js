35eceb3927bfe90d8f40acc8baa5452151f88a9c
Victor Gomez <vimago@gmail.com>
2013-07-11 12:55:16 +0000

    Grader with more tags

--- grader.js
+++ grader.js
@@ -21,10 +21,11 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -36,16 +37,26 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertValidUrl = function(inUrl) {
+  var url = inUrl.toString();
+  if(!validUrl(url)) {
+    console.error('Error: Invalid url');
+    process.exit(1);
+  }
+  return url;
 };
+
+// https://forums.digitalpoint.com/threads/javascript-validation-for-url.242636/
+function validUrl(url){
+  return url.match(/^(ht|f)tps?:\/\/[a-z0-9-\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?$/);
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(html, checksfile) {
+    $ = cheerio.load(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +72,40 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var processUrl = function(checksfile) {
+  return function(result, response) {
+    if (result instanceof Error) {
+      console.error('Error: ' + util.format(response.message));
+      process.exit(1);
+    } else {
+      if(response.statusCode == 200) {
+        displayHtmlChecks(result, checksfile);
+      } else {
+        console.log('Error: ' + response.statusCode);
+      }
+    }
+  };
+};
+
+var displayHtmlChecks = function(html, checksfile) {
+  var checkJson = checkHtml(html, checksfile);
+  var outJson = JSON.stringify(checkJson, null, 4);
+  console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to html file', clone(assertFileExists))
+        .option('-u, --url <url>', 'Url for html file', clone(assertValidUrl))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.file) {
+      html = fs.readFileSync(program.file);
+      displayHtmlChecks(html, program.checks);
+    }
+    if(program.url) {
+      rest.get(program.url).on('complete', processUrl(program.checks));
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
